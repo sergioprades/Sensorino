86e81a050f9c72f35a2bf7908c45a77bfd383e9a
Pablo Fernandez <pablo.fernandez@cscs.ch>
2015-03-05 21:30:59 +0100

    lowercase for enum works now with annotated SerializedName

--- app/src/main/java/org/trecet/nowhere/sensorino/message/MessageType.java
+++ app/src/main/java/org/trecet/nowhere/sensorino/message/MessageType.java
@@ -4,13 +4,20 @@ package org.trecet.nowhere.sensorino.message;
  * Created by edoras on 3/2/15.
  */
 
+import com.google.gson.annotations.SerializedName;
 
 public enum MessageType {
+    @SerializedName("get_device_info")
     GET_DEVICE_INFO,
+    @SerializedName("device_info")
     DEVICE_INFO,
+    @SerializedName("get_sensor_info")
     GET_SENSOR_INFO,
+    @SerializedName("sensor_info")
     SENSOR_INFO,
+    @SerializedName("get_sensor_data")
     GET_SENSOR_DATA,
+    @SerializedName("sensor_data")
     SENSOR_DATA,
     ;
 
